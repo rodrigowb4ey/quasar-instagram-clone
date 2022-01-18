7c92db1b8577fd83cc641f025dc69006e99e3aaf
rodrigowb4ey <rodrigobezerrasaraiva@gmail.com>
2022-01-18 08:06:36 -0300

    Idk, i changed it a lot and kind of lost track lol

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,13 +1,69 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img
+        class="full-width"
+        src="https://i.pinimg.com/originals/a7/57/a7/a757a7e3c7cd29073a24cee6b4a84f9c.jpg">
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        size="lg"
+        round
+        color="grey-10"
+        icon="eva-camera"
+      />
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.caption"
+          label="Caption"
+          class="col col-sm-6"
+          dense
+        />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.location"
+          label="Location"
+          class="col col-sm-6"
+          dense
+        >
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn
+          unelevated
+          rounded
+          color="primary"
+          label="Post Image" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { uid } from 'quasar'
 
-export default defineComponent({
-  name: 'PageCamera'
-})
+export default{
+  name: 'PageCamera',
+  data() {
+    return {
+      post: {
+        id: uid(),
+        caption: '',
+        location: '',
+        photo: null,
+        date: Date.now(),
+      }
+    }
+  }
+}
 </script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,13 +1,141 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Page</h5>
+  <q-page class="constrain q-pa-md">
+
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered>
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://pbs.twimg.com/profile_images/1371630348556177411/H-KKwOtH_400x400.jpg">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">@armin_arlet</q-item-label>
+              <q-item-label caption>
+                {{ post.location }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-img
+            :src="post.imageUrl"
+          />
+          <q-card-section>
+            <div class="">{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ niceDate(post.date) }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://pbs.twimg.com/profile_images/1371630348556177411/H-KKwOtH_400x400.jpg">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">@armin_arlet</q-item-label>
+            <q-item-label caption>
+              Armin Arlet
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
+
+    <q-card
+      v-for="post in posts"
+      :key="post.id"
+      class="card-post q-mb-md"
+      flat
+      bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://pbs.twimg.com/profile_images/1371630348556177411/H-KKwOtH_400x400.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">@armin_arlet</q-item-label>
+          <q-item-label caption>
+            {{ post.location }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-img
+        :src="post.imageUrl"
+      />
+      <q-card-section>
+        <div class="">{{ post.caption }}</div>
+        <div class="text-caption text-grey">{{ niceDate(post.date) }}</div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { date } from 'quasar'
 
-export default defineComponent({
-  name: 'PageHome'
-})
+export default {
+  name: 'PageHome',
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: 'Have you ever seen the ocean?',
+          date: 1642427408372,
+          location: 'Shiganshina Distric, Paradis Island',
+          imageUrl: 'https://i.pinimg.com/originals/a7/57/a7/a757a7e3c7cd29073a24cee6b4a84f9c.jpg',
+        },
+        {
+          id: 2,
+          caption: 'Have you ever seen the ocean?',
+          date: 1642427408372,
+          location: 'Shiganshina Distric, Paradis Island',
+          imageUrl: 'https://i.pinimg.com/originals/a7/57/a7/a757a7e3c7cd29073a24cee6b4a84f9c.jpg',
+        },
+        {
+          id: 3,
+          caption: 'Have you ever seen the ocean?',
+          date: 1642427408372,
+          location: 'Shiganshina Distric, Paradis Island',
+          imageUrl: 'https://i.pinimg.com/originals/a7/57/a7/a757a7e3c7cd29073a24cee6b4a84f9c.jpg',
+        },
+        {
+          id: 4,
+          caption: 'Have you ever seen the ocean?',
+          date: 1642427408372,
+          location: 'Shiganshina Distric, Paradis Island',
+          imageUrl: 'https://i.pinimg.com/originals/a7/57/a7/a757a7e3c7cd29073a24cee6b4a84f9c.jpg',
+        }
+      ]
+    }
+  },
+  methods: {
+    niceDate(value) {
+      return date.formatDate(value, 'MMMM D h:mmA')
+    }
+  }
+}
 </script>
+
+<style lang="sass">
+  .card-post
+    .q-image
+      min-height: 200px
+</style>
